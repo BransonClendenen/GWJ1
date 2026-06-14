95b5e88ceeb70a63e1bf658f67839c687f6ce6b9
Gebsti <shinkuu03@gmail.com>
2026-06-13 19:13:11 -0700

    resaved project

--- gwj-1/Scenes/Game/Towers/red_bullet.gd
+++ gwj-1/Scenes/Game/Towers/red_bullet.gd
@@ -18,5 +18,7 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+func _on_area_2d_area_entered(body):
+	if "Soldier A" in body.name:
+		body.Health -= bulletDamage
+		queue_free()
