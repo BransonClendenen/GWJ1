613fc020c3d3cd923039c48b1c15045b76faa8ea
Branson Clendenen <bclendenen@csub.edu>
2026-06-16 17:47:22 -0700

    Turtle Tank scene

--- gwj-1/Scripts/Game/Enemies/turtler.gd
+++ gwj-1/Scripts/Game/Enemies/turtler.gd
@@ -1,13 +1,41 @@
 extends Enemy
 class_name TurtlerEnemy
 
-# Called when the node enters the scene tree for the first time.
+@export var splitter_scene: PackedScene = preload("res://Scenes/Game/Enemies/splitter.tscn")
+const SPLIT_COUNT = 6
+const SPAWN_SPREAD = 6.0
+
 func _ready():
 	enemy_name = "Turtler"
 	max_hp = 400
-	max_stamina = 200
+	max_stamina = 0
 	max_armor = 200
+	armor_reduction = 0.5
 	speed = 10
 	coin_reward = 100
 	damage_to_base = 100
 	super._ready()
+
+func on_armor_broken():
+	max_stamina = max_hp/2.0
+	current_stamina = max_stamina
+	speed = 15
+	update_stamina_bar()
+	VfxManager.burst(get_parent(), global_position, Color.ORANGE, 24, 0.6)
+	VfxManager.flash(self, Color.ORANGE)
+
+func on_death():
+	if not splitter_scene:
+		push_error("SplitterEnemy: mini_scene not assigned")
+		return
+	
+	for i in range(SPLIT_COUNT):
+		var splitter = splitter_scene.instantiate()
+		get_parent().add_child(splitter)
+		
+		var angle = (TAU / SPLIT_COUNT) * i
+		var offset = Vector2(cos(angle), sin(angle)) * SPAWN_SPREAD
+		splitter.global_position = global_position + offset
+		
+		splitter.set_path_from_index(path_points, path_index)
+		wave_manager.register_enemy(splitter)
